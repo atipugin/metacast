1928c13b75d8a74ade3d8b81c172f74069b99cbd
Alexander Tipugin <atipugin@gmail.com>
2019-04-15 16:47:51 +0300

    Revert "Use `.asset_url` instead of `.asset_path`"
    This reverts commit 49cd0e3745926a458ff0679f8c358c867979395f.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,7 +10,7 @@ class ImageUploader < ApplicationUploader
   end
 
   def default_url
-    ActionController::Base.helpers.asset_url(
+    ActionController::Base.helpers.asset_path(
       File.join(
         'fallback', 'image', [version_name, 'default.png'].compact.join('_')
       )
