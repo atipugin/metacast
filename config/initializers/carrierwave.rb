1e7fe022e63489c6f98a7474e5a36a3d8085a89b
Alexander Tipugin <atipugin@gmail.com>
2019-05-16 00:32:00 +0300

    Use cloudfront

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
   config.storage = :aws
+  config.asset_host = 'https://dlqied8bscuk8.cloudfront.net'
   config.aws_bucket = ENV['S3_BUCKET_NAME']
   config.aws_acl = 'public-read'
   config.aws_credentials = {
